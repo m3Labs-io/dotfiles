f7a3c7131ae3a9fd271f1c4df9826c1faca3dfe0
Magnus McCune <magnus@gbstech.com>
2020-05-12 16:41:48 -0300

    changes to runbook, vimrc

--- runbook.sh
+++ runbook.sh
@@ -7,5 +7,12 @@ git clone https://github.com/*/dotfiles.git
 
 sudo apt-get install tmux
 
+# Kill any existing sessions
+
+tmux kill-server
+
+# Install TPM
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 
